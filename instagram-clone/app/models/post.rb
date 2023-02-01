bb9027ce87c0e11b6749a4887e0ec64ef6249e9f
muoi07052001 <muoi07052001@gmail.com>
2023-02-01 09:35:45 +0000

    Add likes

--- instagram-clone/app/models/post.rb
+++ instagram-clone/app/models/post.rb
@@ -1,12 +1,20 @@
 class Post < ApplicationRecord
-    mount_uploader :image, ImageUploader
-    belongs_to :account
-    
-    before_create :set_active
-    
-    scope :active, -> { where active: true}
-    
-    def set_active
-      self.active = true
-    end
+  default_scope { order(created_at: :desc) }
+  mount_uploader :image, ImageUploader
+  belongs_to :account
+  has_many :likes
+  
+  before_create :set_active
+  
+  scope :active, -> { where active: true}
+  
+  def total_likes
+    0
+  end
+  
+  private
+  
+  def set_active
+    self.active = true
+  end
 end
--- instagram-clone/db/schema.rb
+++ instagram-clone/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_31_182637) do
+ActiveRecord::Schema.define(version: 2023_02_01_084804) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,12 +35,22 @@ ActiveRecord::Schema.define(version: 2023_01_31_182637) do
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "account_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_likes_on_account_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "image"
     t.boolean "active"
     t.integer "account_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
