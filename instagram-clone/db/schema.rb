0c0000b0eea8499d414cbd115efa7cbb6ccf0fdb
Duong Nguyen <muoi07052001@gmail.com>
2023-02-02 09:09:51 +0000

    Add comment function

--- instagram-clone/db/schema.rb
+++ instagram-clone/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_02_015356) do
+ActiveRecord::Schema.define(version: 2023_02_02_084219) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2023_02_02_015356) do
     t.string "website"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "account_id"
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_comments_on_account_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "followers", force: :cascade do |t|
